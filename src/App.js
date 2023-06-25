9e1b83ed6e976787aac1fd502276161a921c2725
dan <81313913+danxczm@users.noreply.github.com>
2023-06-25 10:42:31 +0300

    switch theme done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { ThemeProvider } from "@emotion/react";
-import theme from "@rebass/preset";
-import palette from "./components/styles/palette";
-import { Button, Flex, Text } from "rebass";
+import theme from "./components/Theme";
 import { Global } from "@emotion/react";
+import { Button, Flex, Text } from "rebass";
+import { Label, Switch } from "@rebass/forms";
+import { useState } from "react";
+// import theme from "@rebass/preset";
 
 const GlobalStyles = () => (
   <Global
@@ -19,32 +21,45 @@ const GlobalStyles = () => (
 function App() {
   console.log(`theme: `, {
     theme,
-    palette,
   });
+  const [switched, setSwitched] = useState(false);
+  const toggleSwitch = () => {
+    setSwitched(!switched);
+  };
+  console.log(`switched: `, switched);
+
+  const { colors, fontSize, fontWeight } = theme;
 
   return (
     <ThemeProvider
       theme={{
         theme,
-        palette,
       }}
     >
       <GlobalStyles />
       <Flex
         flexDirection="column"
-        justifyContent="center"
+        justifyContent="flex-start"
         alignItems="center"
-        bg={palette.generalPerfectGray}
-        p={20}
+        bg={switched ? colors.generalSplashBg : colors.generalPerfectGray}
+        p={40}
+        height="100vh"
       >
-        <Text mb={20} fontWeight="bold">
-          Light theme buttons
+        <Label sx={{ padding: "10px" }}>
+          <Switch mr="10px" checked={switched} onClick={() => toggleSwitch()} />
+        </Label>
+        <Text
+          mb={20}
+          fontWeight={fontWeight.normal}
+          color={switched ? "white" : "black"}
+        >
+          {switched ? "Dark" : "Light"} theme buttons
         </Text>
         <Button
           sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          fontWeight="600"
-          color={palette.generalDavysGray}
+          fontSize={fontSize.m}
+          fontWeight={fontWeight.normal}
+          color={switched ? colors.generalGentleGray : colors.generalDavysGray}
           bg="inherit"
           width={340}
           height={50}
@@ -54,10 +69,12 @@ function App() {
         </Button>
         <Button
           sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          fontWeight="600"
-          color={palette.generalGhostWhite}
-          bg={palette.generalPebblePebble}
+          fontSize={fontSize.m}
+          fontWeight={fontWeight.normal}
+          color={
+            switched ? colors.generalPebblePebble : colors.generalGhostWhite
+          }
+          bg={switched ? colors.generalGentleGray : colors.generalPebblePebble}
           width={340}
           height={50}
           m={2}
@@ -66,10 +83,12 @@ function App() {
         </Button>
         <Button
           sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          fontWeight="600"
-          color={palette.generalGhostWhite}
-          bg={palette.generalPebblePebble}
+          fontSize={fontSize.m}
+          fontWeight={fontWeight.normal}
+          color={
+            switched ? colors.generalPebblePebble : colors.generalGhostWhite
+          }
+          bg={switched ? colors.generalGentleGray : colors.generalPebblePebble}
           width={340}
           height={50}
           m={2}
@@ -78,10 +97,12 @@ function App() {
         </Button>
         <Button
           sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          fontWeight="600"
-          color={palette.generalGhostWhite}
-          bg={palette.generalPebblePebble}
+          fontSize={fontSize.m}
+          fontWeight={fontWeight.normal}
+          color={
+            switched ? colors.generalPebblePebble : colors.generalGhostWhite
+          }
+          bg={switched ? colors.generalGentleGray : colors.generalPebblePebble}
           width={340}
           height={50}
           m={2}
@@ -90,79 +111,10 @@ function App() {
         </Button>
         <Button
           sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          fontWeight="600"
-          color={palette.generalGhostWhite}
-          bg={palette.generalSilverSilver}
-          width={340}
-          height={50}
-          m={2}
-        >
-          Get SMS Code
-        </Button>
-      </Flex>
-
-      {/* // ! Dark theme */}
-
-      <Flex
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        bg={palette.generalSplashBg}
-        p={20}
-      >
-        <Text mb={20} fontWeight="bold" color="white">
-          Dark theme buttons
-        </Text>
-        <Button
-          sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          color={palette.generalGentleGray}
-          bg="inherit"
-          width={340}
-          height={50}
-          m={2}
-        >
-          Sign up
-        </Button>
-        <Button
-          sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          color={palette.generalPebblePebble}
-          bg={palette.generalGentleGray}
-          width={340}
-          height={50}
-          m={2}
-        >
-          Login
-        </Button>
-        <Button
-          sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          color={palette.generalPebblePebble}
-          bg={palette.generalGentleGray}
-          width={340}
-          height={50}
-          m={2}
-        >
-          Next
-        </Button>
-        <Button
-          sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          color={palette.generalPebblePebble}
-          bg={palette.generalGentleGray}
-          width={340}
-          height={50}
-          m={2}
-        >
-          Done
-        </Button>
-        <Button
-          sx={{ borderRadius: "10px" }}
-          fontSize={2}
-          color={palette.generalSilverOp50}
-          bg={palette.darkThemeShadesOn}
+          fontSize={fontSize.m}
+          fontWeight={fontWeight.normal}
+          color={switched ? colors.generalSilverOp50 : colors.generalGhostWhite}
+          bg={switched ? colors.darkThemeShadesOn : colors.generalSilverSilver}
           width={340}
           height={50}
           m={2}
---
+++ src/components/Theme.js
@@ -0,0 +1,120 @@
+const theme = {
+  colors: {
+    lightThemeVioletViolet: "#864DD9",
+    lightThemeVioletOp70: "#864DD9B2", // rgba(134, 77, 217, 0.7)
+    lightThemeVioletOp15: "#864DD926", // rgba(134, 77, 217, 0.15)
+    lightThemeVioletOp10: "#864DD91A", // rgba(134, 77, 217, 0.1)
+    lightThemeVioletLightViolet: "#C0A0EF",
+    lightThemeVioletGrayViolet: "#CBC4D5",
+
+    lightThemeWhiteSmoke: "#F5F5F5",
+    lightThemeWhiteSmokeOp0: "#F5F5F500",
+    lightThemeWhite: "#FFFFFF",
+    lightThemeMediumGrayOp20: "#B8B8B833", // rgba(184, 184, 184, 0.2)
+    lightThemeMediumGrayOp30: "#B8B8B84D", // rgba(184, 184, 184, 0.3)
+    lightThemeMediumGrayOp80: "#B8B8B8CC", // rgba(184, 184, 184, 0.8)
+    lightThemeLightGray: "#D1D1D1",
+    lightThemeLightGreen: "#94F0C3",
+    lightThemeGreen: "#16AC62",
+    lightThemeGreenOp10: "#16ac621a", // rgba(22, 172, 98, 0.1)
+    lightThemePink: "#F24B93",
+    lightThemeGoldenEye: "#EC9A22",
+    lightThemeGoldenEyeOp10: "#ec9a221a", // rgba(236, 154, 34, 0.1)
+    lightThemeRed: "#E44D5E",
+
+    generalGhostWhite: "#F7F7F7",
+    generalGhostWhiteOp60: "#F7F7F799",
+    generalGentleGray: "#DADADA",
+    generalPerfectGray: "#E9E9E9",
+    generalPerfectGrayOp0: "#E9E9E900",
+    generalPerfectGrayOp50: "#E9E9E980",
+    generalSpanishGray: "#999999",
+    generalSpanishGrayOp10: "#9999991a", // rgba(153, 153, 153, 0.1)
+    generalBlackShadowOp20: "#00000033",
+    generalBlackShadowOp50: "#00000080",
+    generalPebblePebble: "#404040",
+    generalSilverOp50: "#C4C4C480", // rgba(196, 196, 196, 0.5)
+    generalSilverSilver: "#C4C4C4",
+    generalPebbleOp40: "#40404066", // rgba(64, 64, 64, 0.4)
+    generalMediumGray: "#B8B8B8",
+    generalLightGreen: "#94F0C3",
+    generalDarkGreen: "#0B5933",
+    generalDavysGray: "#5C5C5C",
+    generalSplashBg: "#141414",
+    generalTransparent: "#00000000",
+
+    // skins
+    skinsUniversalBlack: "#262626",
+    skinsStrongViolet: "#6C39B8",
+    skinsLightBlue: "#8D85F4",
+    skinsDarkPink: "#B147E5",
+    skinsTan: "#B8A69C",
+    skinsGold: "#F3BB68",
+    skinsStrongGold: "#F8B630",
+    skinsOrange: "#F37537",
+    skinsDarkOrange: "#F05637",
+    skinsRed: "#DC2F40",
+    skinsLightRed: "#F2749A",
+    skinsMediumRed: "#F03C72",
+    skinsPink: "#E752B6",
+    skinsBlue: "#3736E8",
+    skinsMediumBlue: "#3171EF",
+    skinsSkyBlue: "#2CAFEF",
+    skinsTurquoise: "#2ACCCD",
+    skinsSpringGreen: "#30D99E",
+    skinsChartreuse: "#31D968",
+    skinsYellowGreen: "#76C84D",
+    skinsBlueViolet: "#6347F5",
+    skinsVegasGold: "#CEB55C",
+    skinsBiceGreen: "#10BB58",
+
+    // gradient
+    splashBgGradient: ["#F7F7F733", "#00000000"],
+
+    vipCardBgGradient1: "#FFC851",
+    vipCardBgGradient2: "#EFB933",
+    vipCardBgGradient3: "#FFB81F",
+    vipCardBg: "#FCC54A",
+
+    // dark skins
+    darkSkinsBlueViolet: "#5958E8",
+    darkSkinsSpringGreen: "#18BC83",
+
+    // dark colors
+    darkThemeVioletViolet: "#9E71E1",
+    darkThemeVioletOp70: "#864DD9B2", // rgba(134, 77, 217, 0.7)
+    darkThemeVioletOp20: "#864DD933", // rgba(134, 77, 217, 0.2)
+    darkThemeVioletOp30: "#7a47c64d", // rgba(122, 71, 198, 0.3)
+    darkThemeVioletLightViolet: "#B28CE9",
+
+    darkThemeWhiteSmokeOp0: "#20202000",
+    darkThemeShadesOn: "#6A6A6A",
+    darkThemeShadesOnOp60: "#6A6A6A99",
+    darkThemeCharcoal: "#2F2F2F",
+    darkThemeJet: "#343434",
+    darkThemeBlack: "#141414",
+    darkThemeMediumGrayOp80: "#B8B8B8CC", // rgba(184, 184, 184, 0.8)
+    darkThemeGreen: "#45C787",
+    darkThemePink: "#F24B93",
+    darkThemeGoldenEye: "#F5BC68",
+    darkThemeRed: "#C64E5C",
+    darkThemeLightBlack: "#222222",
+
+    // cardColors
+    cardDarkColor: "#070707",
+  },
+  fontSize: {
+    xs: "8px",
+    s: "12px",
+    m: "16px",
+    l: "20px",
+    xl: "24px",
+  },
+  fontWeight: {
+    light: 200,
+    normal: 400,
+    bold: 600,
+  },
+};
+
+export default theme;
